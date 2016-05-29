0bf4f1b46a3ac66db8cd09056c6baa67071c5461
Alex Gomez <alexgome@ccs.neu.edu>
2016-05-29 14:46:46 -0400

    website file fixes: navigation and updating website

--- public/assignment/views/website/website-edit.controller.client.js
+++ public/assignment/views/website/website-edit.controller.client.js
@@ -7,7 +7,10 @@
         var vm = this;
         vm.userId = $routeParams.userId;
         vm.websiteId = $routeParams.websiteId;
+        vm.website = WebsiteService.findWebsiteById(vm.websiteId);
+        console.log(vm.website);
         vm.deleteWebsite = deleteWebsite;
+        vm.updateWebsite = updateWebsite;
 
         function deleteWebsite(websiteId) {
             var result = WebsiteService.deleteWebsite(websiteId);
@@ -17,5 +20,17 @@
                 vm.error = "Unable to delete website";
             }
         }
+
+        function updateWebsite(wid, website) {
+            var result = WebsiteService.updateWebsite(wid, website);
+            if(result) {
+                $location.url("/user/"+vm.userId+"/website");
+            }
+            else {
+                vm.error = "Unable to update website";
+            }
+        }
+
+
     }
 })();
